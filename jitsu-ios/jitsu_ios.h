19ddda3aea88b6372adc25edc8bb6386187240d5
Leonid Serebryanyy <leosilver@yandex.ru>
2021-06-04 00:43:37 +0300

    Initial Commit

---
+++ jitsu-ios/jitsu_ios.h
@@ -0,0 +1,18 @@
+//
+//  jitsu_ios.h
+//  jitsu-ios
+//
+//  Created by Leonid Serebryanyy on 04.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for jitsu_ios.
+FOUNDATION_EXPORT double jitsu_iosVersionNumber;
+
+//! Project version string for jitsu_ios.
+FOUNDATION_EXPORT const unsigned char jitsu_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <jitsu_ios/PublicHeader.h>
+
+
