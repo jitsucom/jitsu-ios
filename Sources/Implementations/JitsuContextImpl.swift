fc0cdef57892d446272db67b0df19406079d8a15
Leonid Serebryanyy <leosilver@yandex.ru>
2021-06-23 11:37:44 +0300

    gathering device info

--- Sources/Implementations/JitsuContextImpl.swift
+++ Sources/Implementations/JitsuContextImpl.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+//import UIKit // todo: move extracting device info to another class
 
 
 class JitsuContextImpl: JitsuContext {
@@ -24,36 +25,66 @@ class JitsuContextImpl: JitsuContext {
 	
 	// MARK: - -
 	
-	init() { // todo: move from init to separate method?
-		addValues(deviceInfo, for: nil, persist: false)
+	private var deviceInfoProvider: DeviceInfoProvider
+	
+	init() {
+		self.deviceInfoProvider = DeviceInfoProvider()
+		
 		addValues(localeInfo, for: nil, persist: false)
-		addValues(sdkVersion, for: nil, persist: false)
+		addValues(appInformation, for: nil, persist: false)
+		// todo: addValues: accessibility info
+		
+		getDeviceInfo { [weak self] in
+			guard let self = self else {return}
+			if let deviceInfo = self.deviceInfo {
+				self.addValues(deviceInfo, for: nil, persist: false)
+			}
+		}
 	}
 	
-	// todo: fetch values
-	
-	private lazy var deviceInfo: [String: [String: String]] = {
-		return [
-			"parsed_ua":
-				[
-					"device": "iPhone 12",
-					"manufacturer": "Apple",
-					"platform": "iOS",
-					"os": "iOS",
-					"os_version": "14.1",
-					"screen_resolution": "1440x900"
-				]
-		]
+	// MARK: - Automatically generated values
+		
+	private lazy var appInformation: [String: String] = {
+		var info = ["src": "jitsu_ios"]
+		
+		let sdkVersion =  Bundle(for: Self.self).infoDictionary?["CFBundleShortVersionString"] as? String
+		info["sdk_version"] = sdkVersion
+		
+		let mainAppVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+		info["app_version_id"] = sdkVersion // todo: change in spec
+		
+		let mainAppBuildVersion = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
+		info["app_build_id"] = sdkVersion // todo: change in spec
+		
+		let appName = Bundle.main.infoDictionary?["CFBundleName"] as! String
+		info["app_name"] = appName
+		
+		return info
 	}()
+	
+	private var deviceInfo: [String: [String: String]]?
+	
+	func getDeviceInfo(_ completion: @escaping () -> Void) {
+		deviceInfoProvider.getDeviceInfo {[weak self] (deviceInfo) in
+			guard let self = self else {return}
+			self.deviceInfo = [
+				"parsed_ua":
+					[
+						"device_brand": deviceInfo.manufacturer, // e.g. "Apple"
+						"ua_family": deviceInfo.deviceModel, // e.g. "iPhone"
+						"device_model": deviceInfo.deviceName, // e.g. "iPhone 12"
+						"os_family": deviceInfo.systemName, // e.g. "iOS"
+						"os_version": deviceInfo.systemVersion, // e.g. "13.3"
+						"screen_resolution": deviceInfo.screenResolution // e.g "1440x900" // todo: here?
+					]
+			]
+		}
+	}
 	
 	private lazy var localeInfo: [String: String] = {
 		return [
-			"user_language": "en-GB",
+			"user_language": Bundle.main.preferredLocalizations.first ?? Locale.current.languageCode ?? "unknown",
 		]
-	}()
-	
-	private lazy var sdkVersion: [String: String] = {
-		return ["sdk_version": "1.4.1"]
 	}()
 	
 	func values(for eventType: EventType) -> [String : Any] {
